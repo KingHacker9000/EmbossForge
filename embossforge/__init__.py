07ee7065b2091a105cdb49645e7e80a343deca2c
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2026-09-07 02:02:47 -0400

    Use installed package metadata as the version source

--- embossforge/__init__.py
+++ embossforge/__init__.py
@@ -1,3 +1,9 @@
 """EmbossForge: parametric embossing tools."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version("embossforge")
+except PackageNotFoundError:  # pragma: no cover - direct source import before installation
+    __version__ = "0.1.0"
