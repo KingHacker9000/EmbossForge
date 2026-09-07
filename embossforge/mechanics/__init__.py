f713291e14222afbbdb12479c9309fc046f79252
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2026-09-06 23:41:28 -0400

    Export mechanical CAD helpers

--- embossforge/mechanics/__init__.py
+++ embossforge/mechanics/__init__.py
@@ -1,5 +1,13 @@
 """Parametric mechanical components for EmbossForge."""
 
+from .cad import build_cartridge, build_press_parts, build_receiver, export_press_pack
 from .spec import CartridgeSpec, PressSpec
 
-__all__ = ["CartridgeSpec", "PressSpec"]
+__all__ = [
+    "CartridgeSpec",
+    "PressSpec",
+    "build_cartridge",
+    "build_receiver",
+    "build_press_parts",
+    "export_press_pack",
+]
