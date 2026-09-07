f1e16952702f0e48f08662719ab48ce2b8e9cdb3
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2026-09-07 17:25:37 -0400

    Export micro embosser helpers

--- embossforge/mechanics/__init__.py
+++ embossforge/mechanics/__init__.py
@@ -9,6 +9,7 @@ from .cad import (
     mechanical_layout,
     validate_assembly_clearance,
 )
+from .micro import export_micro_press_pack, micro_press_cartridge_spec, micro_press_spec
 from .mini import export_mini_test_pack, mini_cartridge_spec, mini_die_spec, mini_press_spec
 from .spec import CartridgeSpec, PressSpec
 
@@ -26,4 +27,7 @@ __all__ = [
     "mini_press_spec",
     "mini_die_spec",
     "export_mini_test_pack",
+    "micro_press_cartridge_spec",
+    "micro_press_spec",
+    "export_micro_press_pack",
 ]
