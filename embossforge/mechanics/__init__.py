6e230415e866a2988d61862cbec7ce576fa8b0ae
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2026-09-06 23:59:08 -0400

    Export V0.2 layout and collision QA helpers

--- embossforge/mechanics/__init__.py
+++ embossforge/mechanics/__init__.py
@@ -1,6 +1,14 @@
 """Parametric mechanical components for EmbossForge."""
 
-from .cad import build_cartridge, build_press_parts, build_receiver, export_press_pack
+from .cad import (
+    build_assembly_preview,
+    build_cartridge,
+    build_press_parts,
+    build_receiver,
+    export_press_pack,
+    mechanical_layout,
+    validate_assembly_clearance,
+)
 from .spec import CartridgeSpec, PressSpec
 
 __all__ = [
@@ -9,5 +17,8 @@ __all__ = [
     "build_cartridge",
     "build_receiver",
     "build_press_parts",
+    "build_assembly_preview",
+    "mechanical_layout",
+    "validate_assembly_clearance",
     "export_press_pack",
 ]
